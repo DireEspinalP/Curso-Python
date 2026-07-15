907202d77de890de77f4887e8ed88318bd796a2b
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-07-15 00:31:59 -0500

    Funciones Integradas en Python

---
+++ Basico/00_Funciones_Integradas.py
@@ -0,0 +1,143 @@
+
+
+#Listado de funciones integradas en Python
+""""
+abs() -> Devuelve el valor absoluto de un número.
+
+all() -> Devuelve True si todos los elementos de un iterable son verdaderos o si el
+iterable está vacío.
+
+any() -> Devuelve True si algún elemento de un iterable es verdadero. Si el iterable está
+vacío, devuelve False.
+
+bin() -> Convierte un número entero en una cadena binaria.
+
+bool() -> Convierte un valor a un booleano, es decir, True o False.
+
+breakpoints()-> Devuelve una lista de puntos de interrupción activos en el depurador.
+
+bytearray() -> Devuelve un objeto bytearray, que es una secuencia mutable de enteros en el rango
+
+bytes() -> Devuelve un objeto bytes, que es una secuencia inmutable de enteros en el rango 
+
+callable() -> Devuelve True si el objeto parece ser llamable (es decir, si se puede llamar
+
+chr() -> Devuelve el carácter correspondiente al valor Unicode dado.
+
+classmethod() -> Devuelve un método de clase para la clase dada.
+
+compile() -> Compila el código fuente en un objeto de código que se puede ejecutar.
+
+complex() -> Devuelve un número complejo a partir de una cadena o de dos números.
+
+delattr() -> Elimina un atributo de un objeto.
+
+dict() -> Devuelve un diccionario, que es una colección de pares clave-valor.
+
+dir() -> Devuelve una lista de los nombres de los atributos y métodos de un objeto.
+
+divmod() -> Devuelve una tupla que contiene el cociente y el resto de la división de dos números.
+
+enumerate() -> Devuelve un objeto enumerado que contiene pares de índice y valor de un iterable.
+
+eval() -> Evalúa una expresión de Python y devuelve el resultado.
+
+exec() -> Ejecuta código Python dinámicamente.
+
+filter() -> Filtra los elementos de un iterable según una función dada y devuelve un objeto filtrado.
+
+float() -> Convierte un valor a un número de punto flotante.
+
+format() -> Devuelve una cadena formateada según un formato especificado.
+
+frozenset() -> Devuelve un conjunto inmutable, que es una colección de elementos únicos.
+
+getattr() -> Devuelve el valor de un atributo de un objeto.
+
+globals() -> Devuelve un diccionario que representa el espacio de nombres global actual.
+
+hassattr() -> Devuelve True si un objeto tiene un atributo con un nombre dado.
+
+hash() -> Devuelve el valor hash de un objeto, que es un número entero que representa el objeto.
+
+help() -> Invoca el sistema de ayuda interactivo de Python.
+
+hex() -> Convierte un número entero en una cadena hexadecimal.
+
+id() -> Devuelve el identificador único de un objeto.
+
+imput() -> Lee una línea de entrada del usuario y la devuelve como una cadena.
+
+int() -> Convierte un valor a un número entero.
+
+isinstance() -> Devuelve True si un objeto es una instancia de una clase o de una subclase de esa clase.
+
+isubclass() -> Devuelve True si una clase es una subclase de otra clase.
+
+iter() -> Devuelve un iterador para un objeto iterable.
+
+len() -> Devuelve el número de elementos de un objeto iterable.
+
+list() -> Devuelve una lista, que es una colección ordenada y mutable de elementos.
+
+locals() -> Devuelve un diccionario que representa el espacio de nombres local actual.
+
+map() -> Aplica una función a cada elemento de un iterable y devuelve un objeto map.
+
+max() -> Devuelve el valor máximo de un iterable o de varios argumentos.
+
+memoryview() -> Devuelve un objeto memoryview que permite acceder a los datos de un objeto de bytes sin copiarlo.
+
+min() -> Devuelve el valor mínimo de un iterable o de varios argumentos.
+
+next() -> Devuelve el siguiente elemento de un iterador.
+
+object() -> Devuelve un objeto base vacío.
+
+oct() -> Convierte un número entero en una cadena octal.
+
+open() -> Abre un archivo y devuelve un objeto de archivo.
+
+ord() -> Devuelve el valor Unicode de un carácter dado.
+
+pow() -> Devuelve el resultado de elevar un número a una potencia.
+
+print() -> Imprime uno o más objetos en la salida estándar.
+
+property() -> Devuelve un objeto de propiedad que se puede usar para definir atributos de clase.
+
+range() -> Devuelve un objeto range que representa una secuencia de números enteros.
+
+repr() -> Devuelve una cadena que representa un objeto de manera que se pueda evaluar con eval().
+
+reversed() -> Devuelve un iterador que recorre los elementos de un iterable en orden inverso.
+
+round() -> Devuelve un número redondeado a un número especificado de dígitos.
+
+set() -> Devuelve un conjunto, que es una colección de elementos únicos.
+
+setattr() -> Establece un atributo de un objeto con un nombre y un valor dados.
+
+slice() -> Devuelve un objeto slice que representa un rango de índices.
+
+sorted() -> Devuelve una lista ordenada de los elementos de un iterable.
+
+staticmethod() -> Devuelve un método estático para la clase dada.
+
+str() -> Convierte un valor a una cadena.
+
+sum() -> Devuelve la suma de los elementos de un iterable.
+
+super() -> Devuelve un objeto que representa la clase base de una clase derivada.
+
+tuple() -> Devuelve una tupla, que es una colección ordenada e inmutable de elementos.
+
+type() -> Devuelve el tipo de un objeto.
+
+vars() -> Devuelve un diccionario que representa los atributos de un objeto.
+
+zip() -> Devuelve un iterador que agrega elementos de varios iterables.
+
+__import__() -> Permite importar un módulo de manera dinámica.
+
+"""
