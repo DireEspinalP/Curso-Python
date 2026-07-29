f9b871ab9f8ff86f949b89f6d3ac18837c6f97c9
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-07-28 23:57:25 -0500

    Leccion 12 Modulos (Final Basico)

---
+++ Basico/12_modulos.py
@@ -0,0 +1,15 @@
+### Modulos ###
+from my_module import SUMA, PRODUCTO
+#modulo.SUMA(5,3,4)
+SUMA()
+PRODUCTO()
+import my_module
+my_module.SUMA()
+my_module.PRODUCTO()
+
+import math
+print(f"Pi es {math.pi}")
+print(math.pow(2,8))
+
+from math import pi as PI_VALUE
+print(PI_VALUE)
---
+++ Basico/my_module.py
@@ -0,0 +1,12 @@
+def SUMA():
+    num1=int(input("Primer num: "))
+    num2=int(input("Segundo num: "))
+    num3=num1+num2
+    print(f"La SUMA de los dos numeros es {num3}")
+SUMA()
+def PRODUCTO():
+    num1=int(input("Primer num: "))
+    num2=int(input("Segundo num: "))
+    num3=num1*num2
+    print(f"La PRODUCTO de los dos numeros es {num3}")
+   
