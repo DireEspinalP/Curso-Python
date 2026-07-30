0dd8b2318ce2b24d442fb86aa56361de7fec6771
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-07-30 01:19:30 -0500

    Clases

---
+++ POO/01_clases.py
@@ -0,0 +1,12 @@
+## Clases ##
+#Recomendacion PascalCase
+
+# Un objeto es una instancia de clase
+class Celular():
+  marca="Samsung"
+  modelo="S23"
+  camara="48MP"
+
+celular1=Celular()
+celular2=Celular()
+celular3=Celular()
