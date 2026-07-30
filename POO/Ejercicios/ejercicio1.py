f685c165e676c17f13ffce0c977426863919c807
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-07-30 01:50:52 -0500

    Ejercicio1 sin solucion

---
+++ POO/Ejercicios/ejercicio1.py
@@ -0,0 +1,15 @@
+"""
+Crear una clase con el nombre de "Estudiante" 
+que tenga los atributos "Nombre", "Edad", "Grado"
+ademas encargar un metodo que se llame "estudiar" que imprima
+"El estudiante {nombre} esta estudiando
+
+Para trabajar con instancia
+Crear un objeto Estudiante
+y usar un metodo estudiar()
+
+incluso debe interactuar con el usuario para brindar los atributos
+si despues de registrar los atributos coloca "estudiar" 
+usar el metodo estudiar()
+
+"""
