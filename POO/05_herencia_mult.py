7ec9c7999138d9d26042a38d5f2fb21713a8d6c5
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-07-31 00:00:17 -0500

    Herencia multiple

---
+++ POO/05_herencia_mult.py
@@ -0,0 +1,31 @@
+## HERENCIA MULTIPLE ##
+
+class Persona:
+    def __init__(self, nombre,edad, nacionalidad):
+        self.nombre=nombre
+        self.edad=edad
+        self.nacionalidad=nacionalidad
+
+    def hablar(self):
+
+        print("Hola, estoy hablando un poco")
+
+class Artista:
+    def __init__(self, habilidad):
+        self.habilidad=habilidad
+    def mostrar_habilidad(self):
+        return (f"Mi habilidad es {self.habilidad}")
+
+class EmpleadoArtista(Persona,Artista):
+    def __init__(self, nombre, edad, nacionalidad,habilidad,salario,empresa):
+        Persona.__init__(self,nombre, edad, nacionalidad)
+        Artista.__init__(self,habilidad)
+        self.salario=salario
+        self.empresa=empresa
+
+    def presentarse(self):
+         # return f'Hola, soy: {self.nombre},{self.mostrar_habilidad()} y trabajo en {self.empresa}' #Heredo a EmpleadoArtista (clase hijo)
+          print(f'Hola, soy: {self.nombre}, {self.mostrar_habilidad()} y trabajo en {self.empresa}') #Heredo a Artista (clase padre)
+
+dire=EmpleadoArtista("Dire",18,"peruano","aprender rapido",1000,"Github")
+dire.presentarse()
