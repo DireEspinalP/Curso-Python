c2f7bb678a750325555789b4415ba00cdd738ae4
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-07-30 20:34:54 -0500

    Ejercicio I

--- POO/Ejercicios/ejercicio1.py
+++ POO/Ejercicios/ejercicio1.py
@@ -13,3 +13,48 @@ si despues de registrar los atributos coloca "estudiar"
 usar el metodo estudiar()
 
 """
+#Solucion
+
+# MI FORMA
+class Estudiante():
+
+    def estudiar(self):
+        self.nombre = input("\nNombre: ")
+        self.edad = int(input("Edad: "))
+        self.grado = str(input("Grado: "))
+        estudia = str(input("Quieres estudiar?\n"))
+        
+        if (estudia.lower() == "si"):
+            print(f"El estudiante {self.nombre} esta estudiando")
+        else:
+            print(f"El estudiante {self.nombre} NO esta estudiando")
+
+
+student1 = Estudiante()   
+student1.estudiar()
+
+# SOLUCIONARIO
+class Estudiante():
+    def __init__(self,nombre,edad,grado):
+        self.nombre = nombre
+        self.edad =edad
+        self.grado =grado
+    def estudiar(self):
+        print("Estudiando...")
+nombre=input("\n\nDigame su nombre: ")
+edad=input("Ahora su edad: ")
+grado=input("Por ultimo, su grado: ")
+
+estudiante=Estudiante(nombre,edad,grado)
+print(f"""
+    DATOS DEL ESTUDIANTE: \n\n
+    Nombre: {estudiante.nombre}\n
+    Edad:: {estudiante.edad}\n
+    Grado:: {estudiante.grado}\n
+    """)
+
+while True:
+    estudiar=input()
+    if (estudiar.lower()=="estudiar"):
+             estudiante.estudiar()
+    break
