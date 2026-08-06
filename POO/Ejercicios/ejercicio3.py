71289254ac06d429e03939568be261a683b771a6
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-08-06 01:10:18 -0500

    Ejercicio III

---
+++ POO/Ejercicios/ejercicio3.py
@@ -0,0 +1,15 @@
+"""
+---------------CREAR UN JUEGO DE FUSION---------------
+
+El juego consiste en crear personajes un juego y que esos personajes
+se puedan fusionar para formar personajes más poderosos que tengan 
+más poder...
+
+Para ello deberemos cambiar el comportamiento del operador "+" para 
+que cuando los personajes se fusionen, salga un nuevo personaje con
+habilidades mejoradas.
+
+una posible formula es: el promedio de las habilidades de ambos,
+al cuadrado.
+
+"""
