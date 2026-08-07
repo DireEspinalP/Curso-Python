4b0bbca153c37ee7b9cc4031462f1281865ca475
DireEspinalP <espinalpechodiredaniel@gmail.com>
2026-08-07 13:05:29 -0500

    Solucion ejercio III

--- POO/Ejercicios/ejercicio3.py
+++ POO/Ejercicios/ejercicio3.py
@@ -13,3 +13,29 @@ una posible formula es: el promedio de las habilidades de ambos,
 al cuadrado.
 
 """
+
+class Personaje:
+    def __init__(self,nombre,fuerza, velocidad):
+          self.nombre=nombre
+          self.fuerza=fuerza
+          self.velocidad=velocidad
+    def __repr__(self):
+        return f"{self.nombre}(Fuerza: {self.fuerza},Velocidad:  {self.velocidad})"
+    def __add__(self,otro_pj):
+         nuevo_nombre=self.nombre + "/" + otro_pj.nombre
+         nueva_fuerza=round(((self.fuerza + otro_pj.fuerza)/2)**1.5)
+         nueva_velocidad=round(((self.velocidad + otro_pj.velocidad)/2)*1.5)
+         return Personaje(nuevo_nombre,nueva_fuerza,nueva_velocidad)
+    
+goku= Personaje("Goku",100,100)
+vegeta=Personaje("Vegeta",99,99)
+jiren=Personaje("Jiren",130,120)
+
+gogeta=goku+vegeta
+jireta=jiren+vegeta
+
+print(f"\n{gogeta}")
+print(jireta)
+print(f"\n{goku}")
+print(vegeta)
+print(jiren)
